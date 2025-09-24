96acbafb3677d95304873966f4bae5ff348f39dc
wbhemingway <william.b.hemingway@gmail.com>
2025-09-24 16:48:46 -0400

    created player

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import (  # noqa: F401
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
 )
+from player import Player
 
 
 def main():
@@ -15,13 +16,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
