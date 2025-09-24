1412113846cfecf81e43c5d69f31c7dfbd151477
wbhemingway <william.b.hemingway@gmail.com>
2025-09-24 16:56:43 -0400

    player turning added

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)
